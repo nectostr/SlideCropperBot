98002f3f4fdf983015a29a5ec7b90f738742e3d1
unknown <bastinda96@gmail.com>
2019-09-01 21:14:31 +0300

    Not accureate, but working demo

---
+++ bot.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: windows-1251 -*-
+
+import config
+import telebot
+import time
+import openCVTest as IW
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image
+import io
+
+if config.use_proxy:
+    telebot.apihelper.proxy = {'https': 'socks5h://{}:{}@{}:{}'.format(config.proxy_user,
+                                                                      config.proxy_pass,
+                                                                      config.proxy_address,
+                                                                      config.proxy_port)}
+
+bot = telebot.TeleBot(config.token)
+cur_ticket = ""
+tickets = os.listdir(config.ticket_directory)
+adding_ticket = 0
+file_to_write = ''
+deleting_ticket = 0
+sending_now = False
+
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    print('start command incoming')
+    bot.reply_to(message, "I accept slides wia photo and return it with more compatible way")
+
+@bot.message_handler(content_types=["photo"])
+def send_ticket(message):
+    print("got photo")
+    fileID = message.photo[-1].file_id
+    print(message.photo[-1])
+    file_info = bot.get_file(fileID)
+    bytes = bot.download_file(file_info.file_path)
+    slide = np.array(Image.open(io.BytesIO(bytes)))
+    result = IW.cut_image(slide)
+    print("got res")
+    res_byte = io.BytesIO()
+    Image.fromarray(result).save(res_byte, format="JPEG")
+    bot.send_photo(message.chat.id, res_byte.getvalue())
+    time.sleep(1)
+
+#
+# @bot.message_handler(content_types=["text"])
+# def send_ticket(message):
+#     bot.reply_to(message, "Unexpected text")
+
+
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
